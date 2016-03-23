383bff24a500139c402c6f16d44eb67242cf856f
srinadhn <nsrinadh@gmail.com>
2016-03-23 18:06:37 +0530

    Update StoreService.java

--- Store/src/main/java/com/tradeleaves/store/service/StoreService.java
+++ Store/src/main/java/com/tradeleaves/store/service/StoreService.java
@@ -1,7 +1,14 @@
 package com.tradeleaves.store.service;
 
+import java.util.List;
+
+import com.tradeleaves.store.dao.StoreDAO;
+import com.tradeleaves.store.domain.Product;
+
 public class StoreService {
-  public static List<Product> getProducts(String searchCriteria)
+	
+	
+	public static List<Product> getProducts(String searchCriteria)
 	{
 		return StoreDAO.getProducts(searchCriteria);
 	}
